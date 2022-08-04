48611d73617507805d7021bd543a748432be6546
PanAndrei <andrei_kmv@inbox.ru>
2022-08-04 11:14:53 +0300

    Initial Commit

---
+++ StarsAnimation/StarsAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarsAnimationApp.swift
+//  StarsAnimation
+//
+//  Created by Andrei Panasenko on 04.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct StarsAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
